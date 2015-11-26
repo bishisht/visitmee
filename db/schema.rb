57affbb90e7ce4e4ebb520629958238763f11830
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2015-11-26 11:03:31 +0545

    generated home controller. copied files and assets. some tweaks needed.

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,10 @@
 
 ActiveRecord::Schema.define(version: 20150516133018) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "username",   limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
